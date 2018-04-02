576f80772d36123a64e033bfbaf2bfa268237a54
Todd Hagler <todd.hagler@ruths.ai>
2018-04-01 19:14:32 -0500

    1# added map

--- imports/api/rsvp/rsvp.js
+++ imports/api/rsvp/rsvp.js
@@ -5,12 +5,14 @@ import winston from 'winston';
 export const RSVPs = new Mongo.Collection('rsvps');
 
 Meteor.methods({
-    'rsvp.insert'(name,phone,email,msg) {
+    'rsvp.insert'(name,phone,email,msg,attending, count) {
         const doc = {
             name: name,
             phone: phone,
             email: email,
-            msg: msg
+            msg: msg,
+            attending: attending,
+            count: count,
         };
         winston.warn(doc);
         RSVPs.insert(doc);
